9402bd08e862a279869add284610f44e580f8042
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-04-10 16:22:58 -0300

    implemente un validador de usuarios con middlewares

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,6 +3,9 @@ let fs = require('fs')
 let controller = {
     index: (req, res) => {
         res.render("index")
+    },
+    admin: (req, res )=> {
+        res.send(`Bienvenid@ admin ${req.query.user}`)
     }
 }
 
--- src/middlewares/adminUsers.js
+++ src/middlewares/adminUsers.js
@@ -1,9 +1,11 @@
-const ADMIN_USERS = ['Ada, ']
+const ADMIN_USERS = ['Ada', 'Greta', 'Vim', 'Tim']
 
 function adminUsers (req, res, next){
-    if(req.query.user == "Greta"){
+    if(req.query.user == "Greta" || req.query.user == "Ada" || req.query.user == "Vim" || req.query.user == "Tim"){
         next()
     }else{
         res.send("quien te conoce rey")
     }
 }
+
+module.exports = adminUsers 
